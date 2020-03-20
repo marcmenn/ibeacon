9bd0f60eea9689eecf4f48d6415c4c275a955777
Marcus Mennemeier <marcus@urban-mobility.io>
2020-03-20 21:29:58 +0100

    initial POST

--- backend/express/index.js
+++ backend/express/index.js
@@ -2,6 +2,12 @@ import express from 'express'
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.post('/device', express.json(), (req, res) => {
+  if (!req.is('json')) {
+    res.sendStatus(406)
+    return
+  }
+  res.send(`registered ${req.body.deviceId}/${req.body.beaconId}`)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
