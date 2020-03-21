c7bca733ecfff21ac968e333efcfdcc833eceb68
Marcus Mennemeier <marcus@urban-mobility.io>
2020-03-21 16:17:36 +0100

    Publish cluster.diagnostics() in hello

--- backend/src/server/actions/hello.js
+++ backend/src/server/actions/hello.js
@@ -1,8 +1,11 @@
 import { getVersion } from '../../get-version.js'
+import { connect } from '../../database/backend/couchbase/index.js'
 
-export default () => (req, res) => {
+export default () => async (req, res) => {
+  const diag = await connect().diagnostics()
   res.send({
     name: 'ibeacon API',
     version: getVersion(),
+    couchbase: diag,
   })
 }
