251e91c3b6394f960b531a818175bb5ab4d86c92
Lars Menzel <Lars.Menzel@menzelapps.com>
2020-03-21 10:52:10 +0100

    show list of beacons

--- ios/CoChain/ContentView.swift
+++ ios/CoChain/ContentView.swift
@@ -14,35 +14,52 @@ struct Person: Identifiable {
     var id = UUID()
     var name: String
     var infected: Bool
-    var risk: Int
+    var date: Date
     var distance: Float
+    var duration: Int
+}
+
+struct Me: Identifiable {
+    var id = UUID()
+    var infected: Bool
 }
 
 struct ContentView: View {
     @State var persons = [
-        Person(name: "nobody met", infected: false, risk: 0, distance: 0)
+        Person(name: "nobody met", infected: false, date: Date(), distance: 0, duration: 0)
     ]
 
-    @State var me = Person(name: "Me", infected: false, risk: Int(arc4random() % 100 + 1), distance: -1)
+    @State var me = Me(infected: false)
 
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
 
    var body: some View {
        VStack {
-           Text("Contact Chain Tracker").bold().font(.largeTitle).padding(5)
-           Text("\(me.name): \(me.risk)% ").bold().font(.largeTitle).padding(12).background(me.infected ? Color.red: Color.black).foregroundColor(Color.white).cornerRadius(12.0)
-            .padding(100)
+        Image(systemName: "person.circle.fill").font(Font.system(size: 60)).foregroundColor(me.infected ? Color.red : Color.white)
+        Text("\(me.infected ? "krank" : "fit")").bold().font(.largeTitle).padding(12).background(me.infected ? Color.red: Color.black).foregroundColor(Color.white).cornerRadius(12.0)
         if(persons.count > 0) {
             ForEach(persons) { person in
-                Text("\(person.name): \(person.distance)m ").padding(5).foregroundColor(person.infected ? Color.red : Color.black).background(Color(red: 0.9, green: 0.9, blue: 0.9)).cornerRadius(6.0).padding(5)
+                HStack {
+                    Image(systemName: "person.circle.fill").font(Font.system(size: 52)).foregroundColor(Color.white).padding(5)
+                    Text("Date\n\(self.formatedDate(date: person.date)) ").padding(5).foregroundColor(person.infected ? Color.red : Color.black).background(Color(red: 0.9, green: 0.9, blue: 0.9)).cornerRadius(6.0)
+                    Text(String(format: "Distance\n%.2f m", person.distance)).padding(5).foregroundColor(person.infected ? Color.red : Color.black).background(Color(red: 0.9, green: 0.9, blue: 0.9)).cornerRadius(6.0)
+                    Text("Duration\n\(person.duration) min").padding(5).foregroundColor(person.infected ? Color.red : Color.black).background(Color(red: 0.9, green: 0.9, blue: 0.9)).cornerRadius(6.0)
+                }
             }
         }
-        Image(systemName: "heart.circle.fill").font(Font.system(.largeTitle)).foregroundColor(me.infected ? Color.red : Color.white).padding(15)
-        }
-       .onReceive(timer) { _ in
-            self.checkBeacons()
-        }
+    }
+   .onReceive(timer) { _ in
+        self.checkBeacons()
+    }
    }
+
+    func formatedDate(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        formatter.timeStyle = .none
+        return formatter.string(for: date)!
+    }
+
 
     func checkBeacons() {
         let sceneDelegate = UIApplication.shared.connectedScenes.first!.delegate as! SceneDelegate
@@ -53,7 +70,7 @@ struct ContentView: View {
             persons = []
             for index in 0...beacons.count - 1 {
                 print(beacons[index])
-                persons.append(Person(name: "\(beacons[index].major)_\(beacons[index].minor)", infected: false, risk: Int(beacons[index].accuracy * 100), distance: Float(beacons[index].accuracy)))
+                persons.append(Person(name: "\(beacons[index].major)_\(beacons[index].minor)", infected: false, date: Date(), distance: Float(beacons[index].accuracy), duration: 0))
             }
         }
     }
