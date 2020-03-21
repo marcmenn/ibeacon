4766a4d53a09779c230b0b6756adb551354cf93a
Marcus Mennemeier <marcus@urban-mobility.io>
2020-03-21 21:58:39 +0100

    drop couchbase internals from hello

--- backend/src/server/actions/hello.js
+++ backend/src/server/actions/hello.js
@@ -1,13 +1,8 @@
 import { getVersion } from '../../get-version.js'
-import { connect } from '../../database/couchbase.js'
 
 export default async (req, res) => {
-  const diag = await connect().diagnostics()
-  const buckets = await connect().buckets().getAllBuckets()
   res.send({
     name: 'ibeacon API',
     version: getVersion(),
-    couchbase: diag,
-    buckets,
   })
 }
