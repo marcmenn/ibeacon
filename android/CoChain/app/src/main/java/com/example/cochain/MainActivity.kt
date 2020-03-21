1a9e249065697c74ace93bd8f11ff08adebf57b5
Pattrick Hueper <phueper@hueper.net>
2020-03-21 23:11:10 +0100

    add and initially use beacon library

--- android/CoChain/app/src/main/java/com/example/cochain/MainActivity.kt
+++ android/CoChain/app/src/main/java/com/example/cochain/MainActivity.kt
@@ -1,26 +1,224 @@
 package com.example.cochain
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.cochain.ui.TimedBeaconSimulator
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.altbeacon.beacon.BeaconManager
+import org.altbeacon.beacon.BeaconParser
+import org.altbeacon.beacon.Region
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver
+import org.altbeacon.beacon.startup.BootstrapNotifier
+import org.altbeacon.beacon.startup.RegionBootstrap
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), BootstrapNotifier {
+
+    private var regionBootstrap: RegionBootstrap? = null
+    private var backgroundPowerSaver: BackgroundPowerSaver? = null
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val PERMISSION_REQUEST_FINE_LOCATION = 1
+        private const val PERMISSION_REQUEST_BACKGROUND_LOCATION = 2
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // beacon setup
+        //BeaconManager.setDebug(true)
+        val beaconManager = BeaconManager.getInstanceForApplication(this)
+        //setup for iBeacons
+        beaconManager.beaconParsers.clear()
+        beaconManager.beaconParsers.add(BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"))
+
+        // from beacon lib example
+        Log.d(TAG, "setting up background monitoring for beacons and power saving")
+        verifyBluetooth()
+        verifyPermission()
+
+        enableMonitoring()
+
+        // simply constructing this class and holding a reference to it in your custom Application
+        // class will automatically cause the BeaconLibrary to save battery whenever the application
+        // is not visible.  This reduces bluetooth power usage by about 60%
+
+        // simply constructing this class and holding a reference to it in your custom Application
+        // class will automatically cause the BeaconLibrary to save battery whenever the application
+        // is not visible.  This reduces bluetooth power usage by about 60%
+        backgroundPowerSaver = BackgroundPowerSaver(this)
+
+        // If you wish to test beacon detection in the Android Emulator, you can use code like this:
+        BeaconManager.setBeaconSimulator(TimedBeaconSimulator())
+        (BeaconManager.getBeaconSimulator() as TimedBeaconSimulator).createTimedSimulatedBeacons()
+
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_profile, R.id.navigation_query, R.id.navigation_faq))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_profile, R.id.navigation_query, R.id.navigation_faq
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    private fun verifyBluetooth() {
+        try {
+            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+                val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                builder.setTitle("Bluetooth not enabled")
+                builder.setMessage("Please enable bluetooth in settings and restart this application.")
+                builder.setPositiveButton(android.R.string.ok, null)
+                builder.setOnDismissListener(DialogInterface.OnDismissListener {
+                    //finish();
+                    //System.exit(0);
+                })
+                builder.show()
+            }
+        } catch (e: RuntimeException) {
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setTitle("Bluetooth LE not available")
+            builder.setMessage("Sorry, this device does not support Bluetooth LE.")
+            builder.setPositiveButton(android.R.string.ok, null)
+            builder.setOnDismissListener(DialogInterface.OnDismissListener {
+                //finish();
+                //System.exit(0);
+            })
+            builder.show()
+        }
+    }
+
+    private fun verifyPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    if (checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) {
+                            val builder =
+                                AlertDialog.Builder(this)
+                            builder.setTitle("This app needs background location access")
+                            builder.setMessage("Please grant location access so this app can detect beacons in the background.")
+                            builder.setPositiveButton(android.R.string.ok, null)
+                            builder.setOnDismissListener {
+                                requestPermissions(
+                                    arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                                    PERMISSION_REQUEST_BACKGROUND_LOCATION
+                                )
+                            }
+                            builder.show()
+                        } else {
+                            val builder =
+                                AlertDialog.Builder(this)
+                            builder.setTitle("Functionality limited")
+                            builder.setMessage("Since background location access has not been granted, this app will not be able to discover beacons in the background.  Please go to Settings -> Applications -> Permissions and grant background location access to this app.")
+                            builder.setPositiveButton(android.R.string.ok, null)
+                            builder.setOnDismissListener { }
+                            builder.show()
+                        }
+                    }
+                }
+            } else {
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    requestPermissions(
+                        arrayOf(
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                        ),
+                        PERMISSION_REQUEST_FINE_LOCATION
+                    )
+                } else {
+                    val builder =
+                        AlertDialog.Builder(this)
+                    builder.setTitle("Functionality limited")
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons.  Please go to Settings -> Applications -> Permissions and grant location access to this app.")
+                    builder.setPositiveButton(android.R.string.ok, null)
+                    builder.setOnDismissListener { }
+                    builder.show()
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            PERMISSION_REQUEST_FINE_LOCATION -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "fine location permission granted")
+                } else {
+                    val builder =
+                        AlertDialog.Builder(this)
+                    builder.setTitle("Functionality limited")
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons.")
+                    builder.setPositiveButton(android.R.string.ok, null)
+                    builder.setOnDismissListener { }
+                    builder.show()
+                }
+                return
+            }
+            PERMISSION_REQUEST_BACKGROUND_LOCATION -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(
+                        TAG, "background location permission granted"
+                    )
+                } else {
+                    val builder =
+                        AlertDialog.Builder(this)
+                    builder.setTitle("Functionality limited")
+                    builder.setMessage("Since background location access has not been granted, this app will not be able to discover beacons when in the background.")
+                    builder.setPositiveButton(android.R.string.ok, null)
+                    builder.setOnDismissListener { }
+                    builder.show()
+                }
+                return
+            }
+        }
+    }
+
+    fun disableMonitoring() {
+        Log.i(TAG, "disabling beacon scanning")
+        regionBootstrap?.disable()
+        regionBootstrap = null
+    }
+
+    fun enableMonitoring() {
+        Log.i(TAG, "enabling beacon scanning")
+        val region = Region("backgroundRegion", null, null, null)
+        regionBootstrap = RegionBootstrap(this, region)
+    }
+
+    override fun didDetermineStateForRegion(state: Int, region: Region?) {
+        Log.i(TAG, "Current region state is: ${if (state == 1) "INSIDE" else "OUTSIDE (${state})"}")
+    }
+
+    override fun didEnterRegion(region: Region?) {
+        Log.i(TAG, "beacon enter region")
+    }
+
+    override fun didExitRegion(region: Region?) {
+        Log.i(TAG, "beacon exit")
     }
 }
