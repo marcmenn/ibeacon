69692537d07b36d348de307041b2bfed16f4121c
Marcus Mennemeier <marcus@urban-mobility.io>
2020-03-21 16:49:54 +0100

    Add buckets to hello output

--- backend/src/server/actions/hello.js
+++ backend/src/server/actions/hello.js
@@ -3,9 +3,11 @@ import { connect } from '../../database/backend/couchbase/index.js'
 
 export default () => async (req, res) => {
   const diag = await connect().diagnostics()
+  const buckets = await connect().buckets().getAllBuckets()
   res.send({
     name: 'ibeacon API',
     version: getVersion(),
     couchbase: diag,
+    buckets,
   })
 }
