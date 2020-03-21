d802cfe80382f6b64b5858b86e8b2e895f07df83
Peter Lüderitz <peter.luederitz@yellowman.de>
2020-03-21 21:08:43 +0100

    fix connection string

--- backend/src/database/backend/couchbase/options.js
+++ backend/src/database/backend/couchbase/options.js
@@ -1,7 +1,7 @@
 const { CB_USERNAME, CB_PASSWORD, CB_BUCKETNAME, CB_CONNECT_STRING } = process.env
 
 export default {
-  connectString: CB_CONNECT_STRING || 'couchbase://localhost',
+  connectString: CB_CONNECT_STRING || 'couchbase://127.0.0.1?config_total_timeout=60',
   bucketName: CB_BUCKETNAME || 'default',
   username: CB_USERNAME || 'username',
   password: CB_PASSWORD || 'password',
