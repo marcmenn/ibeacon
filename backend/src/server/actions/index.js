1e227664fe9c3c617d7f25985807408664cdc3d5
Marcus Mennemeier <marcus@urban-mobility.io>
2020-03-21 14:12:38 +0100

    fix uncaught express exceptions

--- backend/src/server/actions/index.js
+++ backend/src/server/actions/index.js
@@ -5,12 +5,20 @@ import healthState from './health-state.js'
 import hello from './hello.js'
 import register from './register.js'
 
+const middlewareForAsync = (middleware) => async (req, res, next) => {
+  try {
+    await middleware(req, res, next)
+  } catch (e) {
+    next(e)
+  }
+}
+
 const defineActions = (app, context) => {
-  app.get('/', json, hello(context))
-  app.get('/api', json, hello(context))
-  app.post('/api/device/:deviceId', jsonCombined, register(context))
-  app.post('/api/device/:deviceId/contact', jsonCombined, contact(context))
-  app.post('/api/device/:deviceId/health-state', jsonCombined, healthState(context))
+  app.get('/', json, middlewareForAsync(hello(context)))
+  app.get('/api', json, middlewareForAsync(hello(context)))
+  app.post('/api/device/:deviceId', jsonCombined, middlewareForAsync(register(context)))
+  app.post('/api/device/:deviceId/contact', jsonCombined, middlewareForAsync(contact(context)))
+  app.post('/api/device/:deviceId/health-state', jsonCombined, middlewareForAsync(healthState(context)))
 }
 
 export {
