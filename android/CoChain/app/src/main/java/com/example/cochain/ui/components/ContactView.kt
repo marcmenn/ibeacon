32af0fd316a209e4e5518784cba391bde7476cdf
Pattrick Hueper <phueper@hueper.net>
2020-03-21 16:53:11 +0100

    make ContactView fields/text settable

--- android/CoChain/app/src/main/java/com/example/cochain/ui/components/ContactView.kt
+++ android/CoChain/app/src/main/java/com/example/cochain/ui/components/ContactView.kt
@@ -1,21 +1,45 @@
 package com.example.cochain.ui.components
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.drawable.Drawable
-import android.text.TextPaint
+import android.os.Build
 import android.util.AttributeSet
-import android.view.LayoutInflater
-import android.view.View
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.cochain.R
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-/**
- * TODO: document your custom view class.
- */
 class ContactView : ConstraintLayout {
+
+    private var _date : LocalDate? = null
+    private var _distance : Int? = null
+    private var _duration : Int? = null
+
+    var date: LocalDate?
+        get() = _date
+        @RequiresApi(Build.VERSION_CODES.O)
+        set(value) {
+            _date = value
+            val textViewDate = findViewById<TextView>(R.id.textViewDate)
+            textViewDate.text = "Date:\n ${_date?.format(DateTimeFormatter.ISO_LOCAL_DATE)}"
+        }
+
+    var distance: Int?
+        get() = _distance
+        set(value) {
+            _distance = value
+            val textViewDistance = findViewById<TextView>(R.id.textViewDistance)
+            textViewDistance.text = "Duration:\n ${_distance}"
+        }
+
+    var duration: Int?
+        get() = _duration
+        set(value) {
+            _duration = value
+            val textViewDuration = findViewById<TextView>(R.id.textViewDuration)
+            textViewDuration.text = "Duration:\n ${_duration}"
+        }
 
     constructor(context: Context) : super(context) {
         init()
--- android/CoChain/app/src/main/java/com/example/cochain/ui/query/QueryFragment.kt
+++ android/CoChain/app/src/main/java/com/example/cochain/ui/query/QueryFragment.kt
@@ -1,24 +1,46 @@
 package com.example.cochain.ui.query
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.cochain.R
+import com.example.cochain.ui.components.ContactView
+import java.time.LocalDate
 
 class QueryFragment : Fragment() {
 
+    private val TAG = "QueryFragment"
+
     private lateinit var queryViewModel: QueryViewModel
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         queryViewModel =
-                ViewModelProviders.of(this).get(QueryViewModel::class.java)
-        return inflater.inflate(R.layout.fragment_query, container, false)
+            ViewModelProviders.of(this).get(QueryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_query, container, false)
+        val statusTextView = root.findViewById<TextView>(R.id.textViewStatus)
+        statusTextView.text = "Status:\nFit"
+
+        val contactView = root.findViewById<ContactView>(R.id.contactView)
+        contactView.date = LocalDate.now()
+        contactView.duration = 10
+        contactView.distance = 15
+        val contactView2 = root.findViewById<ContactView>(R.id.contactView2)
+        contactView2.date = LocalDate.now().minusDays(1)
+        contactView2.duration = 5
+        contactView2.distance = 10
+
+        return root
     }
 }
