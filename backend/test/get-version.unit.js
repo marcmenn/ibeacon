2b8cb1c0b9f4a23742f589b3796ca395dcb44666
Marcus Mennemeier <marcus@urban-mobility.io>
2020-03-21 10:15:52 +0100

    ESM: convert get-version.unit

--- backend/test/get-version.unit.js
+++ backend/test/get-version.unit.js
@@ -1,11 +1,14 @@
-import { getVersion } from '../src/get-version'
+import chai from 'chai'
+import { getVersion } from '../src/get-version.js'
+
+const { expect } = chai
 
 describe('getVersion', () => {
-  test('return version number', () => {
+  it('return version number', () => {
     const version = getVersion()
     const numbers = version.split('.').map(Number.parseInt)
 
-    expect(numbers.length).toBe(3)
-    expect(numbers[0]).toBeGreaterThanOrEqual(1)
+    expect(numbers).to.have.lengthOf(3)
+    expect(numbers[0]).to.be.at.least(1)
   })
 })
