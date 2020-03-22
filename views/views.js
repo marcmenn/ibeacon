eaae9005d6c4781ce1ad86fb2e3856efd71a123e
Peter Lüderitz <peter.luederitz@yellowman.de>
2020-03-22 08:37:06 +0100

    add contact by beacon view

--- views/views.js
+++ views/views.js
@@ -1,10 +1,20 @@
 export const contactReport = {
   map(doc, meta) {
-    const { type, deviceId, payload, timestamp: _timestamp } = doc
+    const { type, payload, timestamp: _timestamp } = doc
     if (type === 'contact') {
       const { beaconId, contactedBeaconId, timestamp = _timestamp } = payload
       emit([beaconId, contactedBeaconId, timestamp])
       emit([contactedBeaconId, beaconId, timestamp])
+    }
+  }
+}
+export const contactByBeacon = {
+  map(doc, meta) {
+    const { type, deviceId, payload, timestamp: _timestamp } = doc
+    if (type === 'contact') {
+      const { beaconId, contactedBeaconId, timestamp = _timestamp, distance } = payload
+      emit(beaconId, { type: 'to', beaconId: contactedBeaconId, timestamp, deviceId, distance })
+      emit(contactedBeaconId, { type: 'from', beaconId, timestamp, deviceId, distance })
     }
   }
 }
