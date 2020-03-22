369e4db7531afdde6abdbc8e0d5a0098f41a0e6e
Marcus Mennemeier <marcus@urban-mobility.io>
2020-03-22 13:22:35 +0100

    reduce for contactReport view

--- views/views.js
+++ views/views.js
@@ -1,10 +1,83 @@
 export const contactReport = {
   map(doc, meta) {
-    const { type, payload, timestamp: _timestamp } = doc
+    const { type, payload, timestamp: serverTimestamp } = doc
     if (type === 'contact') {
-      const { beaconId, contactedBeaconId, timestamp = _timestamp } = payload
-      emit([beaconId, contactedBeaconId, timestamp])
-      emit([contactedBeaconId, beaconId, timestamp])
+      const { beaconId, contactedBeaconId, timestamp: clientTimestamp, distance = 10 } = payload
+      let ms
+      try {
+        ms = (new Date(clientTimestamp)).getTime()
+      } catch (e) {
+        ms = (new Date(serverTimestamp)).getTime()
+      }
+      const start = new Date(ms)
+
+      const emitEvents = (beaconId, contactedBeaconId) => {
+        emit([beaconId, start.getUTCFullYear(), start.getUTCMonth(), start.getUTCDate()], {
+          contact: contactedBeaconId,
+          ms,
+          distance,
+        })
+      }
+      emitEvents(beaconId, contactedBeaconId)
+      emitEvents(contactedBeaconId, beaconId)
+    }
+  },
+
+  reduce(key, values, rereduce) {
+    const INTERVAL = 10 * 60000 // amount of minutes for grouping events
+    const doc2reduce = ({ contact, ms, distance }) => {
+      const result = {}
+      result[contact] = [{
+        start: ms,
+        end: ms,
+        n: 1,
+        minDistance: distance,
+        maxDistance: distance,
+        sumDistance: distance,
+      }]
+      return result
+    }
+
+    const findIndex = (array, predicate) => {
+      for (let i = 0; i < array.length; i += 1) {
+        if (predicate(array[i])) return i;
+      }
+      return -1
+    }
+
+    const reduceIntervals = (result, value) => {
+      const i = findIndex(result, (interval) => value.start <= interval.end + INTERVAL)
+      if (i < 0) {
+        result.push(value)
+      } else {
+        const interval = result[i]
+        if (interval.start > value.end + INTERVAL) {
+          result.splice(i, 0, value)
+        } else {
+          interval.start = Math.min(interval.start, value.start)
+          interval.end = Math.max(interval.end, value.end)
+          interval.n += 1
+          interval.minDistance = Math.min(interval.minDistance, value.minDistance)
+          interval.maxDistance = Math.max(interval.maxDistance, value.maxDistance)
+          interval.sumDistance += value.sumDistance
+        }
+      }
+      return result
+    }
+
+    const reduceContacts = (result, value) => {
+      for (const contact in value) {
+        if (Object.prototype.hasOwnProperty.call(value, contact)) {
+          result[contact] = value[contact].reduce(reduceIntervals, result[contact] || [])
+        }
+      }
+      return result
+    }
+
+    if (rereduce) {
+      return values.reduce(reduceContacts)
+    } else {
+      return values.map(doc2reduce).reduce(reduceContacts)
     }
   }
 }
