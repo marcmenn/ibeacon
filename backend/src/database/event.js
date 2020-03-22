7fd3b488f875b700c94c2f848b0d24387f1fc3ab
Peter Lüderitz <peter.luederitz@yellowman.de>
2020-03-22 07:54:59 +0100

    fix id creation

--- backend/src/database/event.js
+++ backend/src/database/event.js
@@ -4,7 +4,7 @@ import { createId } from '../utility/create-id.js'
 import { collection } from './couchbase.js'
 
 const saveEvent = async (type, deviceId, payload) => {
-  const id = createId
+  const id = createId()
   const event = { type, deviceId, timestamp: timeNow(), payload }
   await collection().insert(id, event)
   return { id, ...event }
