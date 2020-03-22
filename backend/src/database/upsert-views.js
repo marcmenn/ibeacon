81611c44f0b7b091bb46334e8123b262d025db12
Peter Lüderitz <peter.luederitz@yellowman.de>
2020-03-22 09:21:58 +0100

    add constant for views design doc

--- backend/src/database/upsert-views.js
+++ backend/src/database/upsert-views.js
@@ -1,9 +1,11 @@
 import fs from 'fs'
 import { bucket, close } from './couchbase.js'
 
+const VIEW_DESIGN_DOC = 'views'
+
 const run = async () => {
   const ddoc = JSON.parse(fs.readFileSync('./views/build/views.json'))
-  ddoc.name = 'views'
+  ddoc.name = VIEW_DESIGN_DOC
   await bucket().viewIndexes().upsertDesignDocument(ddoc)
 }
 
